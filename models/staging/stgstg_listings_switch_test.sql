7a90fc4d7c571c651268333f2f6b50edc0be07a2
Azee <kalesha9099@gmail.com>
2026-06-26 19:53:16 +0530

    PDE-5858: stg omdified

---
+++ models/staging/stgstg_listings_switch_test.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='view') }}
+
+select
+    listing_id,
+    host_id,
+    property_type,
+    city,
+    upper(SUBSTRING(city,1,3)) as shorten_city_name_switch_test,
+    price_per_night,
+    is_available
+from {{ ref('listings') }}
+
